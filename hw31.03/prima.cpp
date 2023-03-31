56111a9b4331566317091db948ddf014b5b6bbd9
Рената Сулейманова <125664248+RenataSul@users.noreply.github.com>
2023-03-31 21:17:55 +0300

    Create prima.cpp

---
+++ hw31.03/prima.cpp
@@ -0,0 +1,63 @@
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+// структура для представления графа в виде списка смежности
+
+struct Graph {
+    int V; // количество вершин
+    vector<vector<pair<int, int>>> adj; // вектор смежности
+
+    Graph(int V) : V{ V }, adj(V) {} // конструктор
+
+    void add_edge(int u, int v, int w) {
+        adj[u].emplace_back(v, w);
+        adj[v].emplace_back(u, w);
+    }
+};
+
+int prim_algorithm(const Graph& graph) {
+    int min_cost = 0; // стоимость минимального остовного дерева
+    vector<bool> visited(graph.V, false); // массив посещенных вершин
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq; // очередь с приоритетами {вес_ребра, вершина}
+    pq.emplace(0, 0); // начинаем с любой вершины (в данном случае с вершины 0) и веса 0
+
+    while (!pq.empty()) { // пока очередь не пуста
+        int u = pq.top().second; // вершина с наименьшим весом
+        int w = pq.top().first; // сохраняем вес текущего ребра
+        pq.pop();
+        if (visited[u]) { // если вершина уже посещена, то перейти к следующей итерации
+            continue;
+        }
+        visited[u] = true; // отметить вершину как посещенную
+        min_cost += w; // добавить вес ребра к стоимости минимального остовного дерева
+        for (auto& [v, w] : graph.adj[u]) { // проходим по всем соседям вершины
+            if (!visited[v]) { // если соседняя вершина не посещена
+                pq.emplace(w, v); // добавляем ребро в очередь
+            }
+        }
+    }
+    return min_cost;
+}
+
+int main() {
+    int n = 5; // количество вершин
+    Graph graph(n); // создаем граф
+
+    // добавляем ребра графа
+    graph.add_edge(0, 1, 2);
+    graph.add_edge(0, 3, 6);
+    graph.add_edge(1, 2, 3);
+    graph.add_edge(1, 3, 8);
+    graph.add_edge(1, 4, 5);
+    graph.add_edge(2, 4, 7);
+    graph.add_edge(3, 4, 9);
+
+    cout << "Minimum Weight: " << prim_algorithm(graph) << endl;
+
+    return 0;
+}
